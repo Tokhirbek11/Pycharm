e31aebfac5b248aaedbaf1e2bf3643e95a09e668
TokhirbekOrifjonov <92422341+TokhirbekOrifjonov@users.noreply.github.com>
2021-10-15 10:34:45 +0500

    iam

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-print("Hello world")
-def toby(letter):
-    print(letter)
-
-toby("hello")
-
-def hi(word):
-    print(word)
-
-hi(12)
-print("fdf")
-def home(address):
-    print(address)
-
-    home("andijan")
-    print(232323)
-
-    print (12121212121)
 
 
 
